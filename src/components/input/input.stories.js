8e2cb385f42df9eb1f00a53d5a4e7af779f843c1
cboenke <clara.boenke@web.de>
2021-01-25 17:44:55 +0100

    Add input

---
+++ src/components/input/input.stories.js
@@ -0,0 +1,9 @@
+import "./input.css";
+import input from "./input-email.html";
+
+export default {
+  title: "Components/Input",
+  parameters: { layout: "centered" },
+};
+
+export const email = () => input;
